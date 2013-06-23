89632453a7811afad1c7b7dee0f3c72676134d37
Thomas <thomas.broust@gmail.com>
2013-06-23 19:36:34 +0200

    Raw Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachmtgCom::Application.config.secret_token = '117c07793ff1d539c9597aaec035d7d084881c7856897702dfad4d06c92677191c51b2b348ca294ebe85b99b7a4eaf857f9b11d1d90cf8b930fe2d71632457b5'
