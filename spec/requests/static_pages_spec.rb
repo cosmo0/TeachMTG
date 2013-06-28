8192f44dca9ed2b6fa77cd85138bf2151c4e79bb
Thomas <thomas.broust@gmail.com>
2013-06-28 20:33:32 +0200

    Gets the contents from the static home page to /static_pages/home

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,14 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
+    it "should have the h1 'How to teach Magic: The Gathering'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', :text => 'How to teach Magic: The Gathering')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title 'How to teach Magic: The Gathering'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
+      page.should have_selector('How to teach Magic: The Gathering',
                         :text => "Home")
     end
   end
