a2d055f2942e615395ced41150fb02f10140e77e
Thomas <thomas.broust@gmail.com>
2013-07-25 23:11:06 +0200

    Files cleanup

--- app/models/card.rb
+++
@@ -1,7 +0,0 @@
-class Card < ActiveRecord::Base
-  
-  attr_accessible :id, :name, :quantity
-  
-  belongs_to :deck
-  
-end
--- app/models/deck.rb
+++
@@ -1,7 +0,0 @@
-class Deck < ActiveRecord::Base
-  
-  attr_accessible :id, :name, :intro, :description
-  
-  has_many :cards
-  
-end
