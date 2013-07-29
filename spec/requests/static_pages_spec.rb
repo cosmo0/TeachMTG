d18d15dae356742c16f74d0c1dbf8a5fc0d0d0ba
Thomas <thomas.broust@gmail.com>
2013-07-29 23:12:03 +0200

    rspec tests refactored

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,72 +4,97 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: page_title) }
+  end
+
   describe "Home page" do
     before { visit home_path }
-    it { should have_selector('h1', :text => 'How do I teach Magic: The Gathering') }
-    it { should have_selector('title', :text => "How to teach Magic: The Gathering") }
+    let(:heading) { 'How do I teach Magic: The Gathering' }
+    let(:page_title) { 'How to teach Magic: The Gathering' }
+
+    it_should_behave_like "all static pages"
   end
 
   # BASICS
 
   describe "Basics page" do
     before { visit basics_path }
-    it { should have_selector('h1', :text => /basic/i) }
-    it { should have_selector('title', :text => /basic/i) }
+    let(:heading) { /basic/i }
+    let(:page_title) { /basic/i }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "World page" do
     before { visit world_path }
-    it { should have_selector('h1', :text => /world/i) }
-    it { should have_selector('title', :text => /world/i) }
+    let(:heading) { /world/i }
+    let(:page_title) { /world/i }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Philosophies page" do
     before { visit philosophies_path }
-    it { should have_selector('h1', :text => /philosophies/i) }
-    it { should have_selector('title', :text => /philosophies/i) }
+    let(:heading) { /philosophies/i }
+    let(:page_title) { /philosophies/i }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Rules page" do
     before { visit rules_path }
-    it { should have_selector('h1', :text => /rules/i) }
-    it { should have_selector('title', :text => /rules/i) }
+    let(:heading) { /rules/i }
+    let(:page_title) { /rules/i }
+
+    it_should_behave_like "all static pages"
   end
 
   # DECKS
 
   describe "Decks page" do
     before { visit decks_path }
-    it { should have_selector('h1', :text => /decks/i) }
-    it { should have_selector('title', :text => /decks/i) }
+    let(:heading) { /decks/i }
+    let(:page_title) { /decks/i }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Beginners page" do
     before { visit beginners_path }
-    it { should have_selector('h1', :text => /beginners/i) }
-    it { should have_selector('title', :text => /beginners/i) }
+    let(:heading) { /beginners/i }
+    let(:page_title) { /beginners/i }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Advanced page" do
     before { visit advanced_path }
-    it { should have_selector('h1', :text => /advanced/i) }
-    it { should have_selector('title', :text => /advanced/i) }
+    let(:heading) { /advanced/i }
+    let(:page_title) { /advanced/i }
+
+    it_should_behave_like "all static pages"
   end
 
   # DRAFTING
 
   describe "Drafting page" do
     before { visit drafting_path }
-    it { should have_selector('h1', :text => /drafting/i) }
-    it { should have_selector('title', :text => /drafting/i) }
+    let(:heading) { /drafting/i }
+    let(:page_title) { /drafting/i }
+
+    it_should_behave_like "all static pages"
   end
 
   # INFOS
 
   describe "Resources page" do
     before { visit resources_path }
-    it { should have_selector('h1', :text => /resources/i) }
-    it { should have_selector('title', :text => /resources/i) }
+    let(:heading) { /resources/i }
+    let(:page_title) { /resources/i }
+
+    it_should_behave_like "all static pages"
   end
 
 end
