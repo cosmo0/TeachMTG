879b21a7a07a5463332fc8241b54a6be9631158f
Thomas <thomas.broust@gmail.com>
2013-10-07 23:06:23 +0200

    Edit form for deck - decklist field added

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -4,6 +4,7 @@ class Deck < ActiveRecord::Base
   has_many :cards
 
   attr_accessible :category, :slug, :title, :cards
+  attr_accessor :decklist
 
   friendly_id :slug
 
@@ -21,6 +22,38 @@ class Deck < ActiveRecord::Base
   before_save do
     self.slug.downcase!
     self.category.upcase!
+    # set_decklist
   end
+
+  # list of cards for text areas
+  def decklist
+    list = ""
+    self.cards.each do |c|
+      list += "#{c.quantity}x #{c.name}\n"
+    end
+    logger.debug("DECKLIST READER " + list)
+    @decklist = list
+    list
+  end
+
+  # Saves the decklist as Cards model
+  def decklist=(value)
+    logger.debug("DECKLIST WRITER " + value)
+    @decklist = value
+    list = Array.new
+    value.split(" ").each do |c|
+      list << c
+    end
+  end
+
+  # def set_decklist
+  #   logger.debug("DECKLIST SET " + self.decklist)
+  #   self.cards.build unless self.cards.present?
+  #   list = self.decklist.split('\n')
+  #   list.each do |item|
+  #     # card = item.split('x ')
+  #     # self.cards.create(name: card.last, quantity: card.first)
+  #   end
+  # end
 
 end
