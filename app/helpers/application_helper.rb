2f791d19698ab1b9d393cdf45b744f224f4085a9
Thomas Broust <thomas.broust@gmail.com>
2013-12-30 22:09:05 +0100

    Fix for decks without some data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   def markdown(text)
+    if text.nil? || text.empty? then return "No description available" end
     md = Redcarpet::Markdown.new(Redcarpet::Render::HTML, { autolink: true, filter_html: true, hard_wrap: true })
     md.render(text).html_safe
   end
