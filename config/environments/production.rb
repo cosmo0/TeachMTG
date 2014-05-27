384822497c94c8c334fdcfd7fab2c6186bda144c
Thomas Broust <thomas.broust@gmail.com>
2014-05-27 16:42:00 +0200

    Precompilation of JS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ TeachmtgCom::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( markdown.converter.js markdown.sanitizer.js )
+  config.assets.precompile += %w( markdown.converter.js markdown.sanitizer.js markdown.editor.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
